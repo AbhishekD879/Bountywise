cd11718968dad8c0064a559098f6ba951a7cc67f
Rajdiwate <127538240+Rajdiwate@users.noreply.github.com>
2024-09-18 18:55:26 +0530

    bug fix (solved prop sending bug while conditional rendering)

--- src/app/private/dashboard/layout.tsx
+++ src/app/private/dashboard/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 import BountyWiseSidebar from "./_components/BountywiseDashboardSidebar";
 import { useState, cloneElement, isValidElement } from "react";
+import MyBounties from "./@myBounties/page"
+import Dashboard from "./page"
 
 export default function DashboardLayout({
-  children,
   analytics,
   messages,
-  myBounties,
   payments,
 }: Readonly<{
   children: React.ReactNode;
@@ -17,21 +17,7 @@ export default function DashboardLayout({
 }>) {
   const [userType, setUserType] = useState<"poster" | "hunter">("poster");
   const [activeTab, setActiveTab] = useState("dashboard");
-
-  // Ensure that `children` is treated as ReactElement with specific props
-  const childrenWithProps = isValidElement(children)
-    ? cloneElement(children as React.ReactElement<{ userType: string }>, {
-        userType,
-      })
-    : children;
-    
-  // Ensure that `myBounties` is treated as ReactElement with specific props  
-  const myBountiesWithProps = isValidElement(myBounties)
-    ? cloneElement(myBounties as React.ReactElement<{ userType: string }>, {
-        userType,
-      })
-    : myBounties;
-
+  
   return (
     <div className="flex h-[calc(100%_-_76px)]">
       <div className="h-full">
@@ -43,10 +29,10 @@ export default function DashboardLayout({
         />
       </div>
       <div className="flex-1 bg-[#ffffff] p-6 ">
-        {activeTab === "dashboard" && isValidElement(children) && childrenWithProps}
+        {activeTab === "dashboard" && <Dashboard userType={userType}/>}
         {activeTab === "analytics" && analytics}
         {activeTab === "messages" && messages}
-        {activeTab === "mybounties" && isValidElement(myBounties) && myBountiesWithProps}
+        {activeTab === "mybounties" && <MyBounties userType={userType}/>}
         {activeTab === "payments" && payments}
       </div>
     </div>
