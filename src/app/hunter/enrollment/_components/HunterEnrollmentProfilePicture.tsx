3aaacda14de0f579b571c131c5b9f09cd322282b
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2024-09-30 16:58:58 +0530

    fixed-stroke width

--- src/app/hunter/enrollment/_components/HunterEnrollmentProfilePicture.tsx
+++ src/app/hunter/enrollment/_components/HunterEnrollmentProfilePicture.tsx
@@ -72,7 +72,7 @@ export default function HunterEnrollmentProfilePicture({
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="white"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
               >
