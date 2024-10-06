2b041759bec03998c495c4f14c25d39f36f64a83
Rajdiwate <127538240+Rajdiwate@users.noreply.github.com>
2024-10-06 12:43:32 +0530

    dashboard width fix

--- src/app/private/dashboard/layout.tsx
+++ src/app/private/dashboard/layout.tsx
@@ -1,12 +1,10 @@
-import {  ReactNode } from 'react'
+import { ReactNode } from 'react'
 import BountyWiseSidebar from './_components/BountywiseDashboardSidebar'
-export default function DashboardLayout({children}: Readonly<{children : ReactNode}>) {
+export default function DashboardLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className='flex h-[calc(100%_-_76px)] gap-10  '>
-      
-        <BountyWiseSidebar />
-        <div className=' overflow-auto my-3 '>{children}</div>
-      </div>
-    
+    <div className='flex h-[calc(100%_-_76px)]   '>
+      <BountyWiseSidebar />
+      <div className=' overflow-auto my-3 w-[calc(100%_-_256px)] mx-5  '>{children}</div>
+    </div>
   )
 }
