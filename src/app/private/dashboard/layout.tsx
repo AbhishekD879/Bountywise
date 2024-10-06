cf6469c3028beb183f2e0b6b4770b9ca53ef3060
Rajdiwate <127538240+Rajdiwate@users.noreply.github.com>
2024-10-06 13:07:42 +0530

    flex-1

--- src/app/private/dashboard/layout.tsx
+++ src/app/private/dashboard/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children }: Readonly<{ children: React
   return (
     <div className='flex h-[calc(100%_-_76px)]'>
       <BountyWiseSidebar />
-      <div className=' overflow-auto my-3 w-[calc(100%_-_256px)] mx-5'>{children}</div>
+      <div className=' overflow-auto my-3 flex-1 mx-5'>{children}</div>
     </div>
   )
 }
